c37c47fa0bc60683e4e552bc0dc31f3f83a1b16a
htimac <htimac@htimacdeiMac.local>
2016-07-13 16:34:17 +0800

    优化Postgresql

--- database/postgreql.go
+++ database/postgreql.go
@@ -2,10 +2,12 @@ package database
 
 import (
 	"database/sql"
-	// aolog "github.com/artwebs/aogo/log"
-	_ "github.com/lib/pq"
 	"strconv"
 	"strings"
+
+	aolog "github.com/artwebs/aogo/log"
+
+	_ "github.com/lib/pq"
 )
 
 func init() {
@@ -14,6 +16,26 @@ func init() {
 
 type Postgresql struct {
 	Driver
+}
+
+func (this *Postgresql) QueryRowNoConn(s string, args ...interface{}) (map[string]string, error) {
+	defer this.Reset()
+	this.limit = "1"
+	var result map[string]string
+	s = this.addLimit(s)
+	rows, err := this.QueryNoConn(s, args...)
+	aolog.InfoTag(this, rows)
+	if err != nil {
+		aolog.InfoTag(this, err)
+		return result, err
+	}
+	if len(rows) > 0 {
+		result = rows[0]
+	} else {
+		result = map[string]string{}
+	}
+	return result, nil
+
 }
 
 func (this *Postgresql) QueryNoConn(s string, args ...interface{}) ([]map[string]string, error) {
